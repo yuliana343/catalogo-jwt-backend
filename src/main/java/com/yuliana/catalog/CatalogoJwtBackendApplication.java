c79900c42190bffc4611d6c3160d174b9a7c9a44
Yuliana Loco <231179@unamba.edu.pe>
2026-01-15 10:27:52 -0500

    Yuliana Loco - Proyecto inicial Spring Boot (Java 21)

---
+++ src/main/java/com/yuliana/catalog/CatalogoJwtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yuliana.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoJwtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoJwtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuliana/catalog/CatalogoJwtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuliana.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoJwtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
