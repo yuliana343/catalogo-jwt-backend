6c9393831860f394a268a4d1c1e41f2defd3717c
Yuliana Loco <231179@unamba.edu.pe>
2026-01-15 11:21:07 -0500

    Yuliana LV - Controladores REST con CORS

--- src/main/java/com/yuliana/catalog/controllers/CategoryController.java
+++ src/main/java/com/yuliana/catalog/controllers/CategoryController.java
@@ -1,5 +1,24 @@
 package com.yuliana.catalog.controllers;
 
+import com.yuliana.catalog.entities.Category;
+import com.yuliana.catalog.services.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CategoryController {
 
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Category> list() {
+        return service.findAll();
+    }
 }
--- src/main/java/com/yuliana/catalog/controllers/ProductController.java
+++ src/main/java/com/yuliana/catalog/controllers/ProductController.java
@@ -1,5 +1,29 @@
 package com.yuliana.catalog.controllers;
 
+import com.yuliana.catalog.entities.Product;
+import com.yuliana.catalog.services.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Product> all() {
+        return service.findAll();
+    }
+
+    @GetMapping("/category/{id}")
+    public List<Product> byCategory(@PathVariable Long id) {
+        return service.findByCategoryId(id);
+    }
 }
