c833bc0cbbb287601aede5926d8a60a39d8f19fb
Yuliana Loco <231179@unamba.edu.pe>
2026-01-15 11:15:59 -0500

    Entidad User, Category y Product implementadas

--- src/main/java/com/yuliana/catalog/entities/Product.java
+++ src/main/java/com/yuliana/catalog/entities/Product.java
@@ -1,5 +1,58 @@
 package com.yuliana.catalog.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Category category;
+
+    public Product() {}
+
+    public Long getId() {
+        return id;
+    }
+ 
+    public void setId(Long id) {
+        this.id = id;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public double getPrice() {
+        return price;
+    }
+ 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+ 
+    public Category getCategory() {
+        return category;
+    }
+ 
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
